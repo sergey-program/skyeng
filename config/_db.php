18ce9c6ea99b4c69030da428a1b139a9e193fba9
Sergey Vasilkov <sergey.program@gmail.com>
2016-08-03 18:38:36 +0300

    fix: clear

--- config/_db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=git.skyeng',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-];
